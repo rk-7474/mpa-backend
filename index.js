50c527069041cfe43e2ee03c2287c3afa83801fd
rk-7474 <raggio271@gmail.com>
2023-12-06 14:34:04 +0100

    modified:   index.js 	modified:   package.json 	modified:   treni.js

--- index.js
+++ index.js
@@ -2,34 +2,66 @@ import express from 'express';
 import getNews from './news.js'
 import { getTrain, getTratta } from './treni.js';
 
-const timestamp = () => new Date().getTime();
+// const timestamp = () => new Date().getTime();
 
 const app = express();
 
 const PORT = 3000;
 
-app.get("/news", async (req, res) => { //http://localhost:3000/news
-	const data = await getNews();
-   	res.send(JSON.stringify(data));
+app.get("*", async (req, res) => { //http://localhost:3000/news
+	switch (req.path) {
+		case "/news":
+			var f = getNews();
+			break;
+		case "/treni":
+			var f = getTratta(req.query.tratta);
+			break;
+		case "/info":
+			var f = getTrain(req.query.stazione, req.query.numero);
+			break;
+	}
+
+	try {
+		var data = await f;
+		if (data == undefined) {
+			console.log("Errore sconosciuto in", req.path, req.query?.stazione, req.query?.numero, req.query?.tratta)
+			res.status(500);
+			res.send()
+			return
+		} 
+		res.send(JSON.stringify(data));
+	} catch(e) {
+		res.status(500);
+		res.send()
+		console.log(e);
+	}
+
 });
 
-app.get("/treni", async (req, res) => { //http://localhost:3000/treni?tratta=2710/2715
-	const data = await getTratta(req.query.tratta);
-   	res.send(JSON.stringify(data));
-});
+app.listen(PORT, () => console.log("Server started"));
 
-app.get("/info", async (req, res) => { //http://localhost:3000/info?stazione=2710&numero=16801
-	const data = await getTrain(req.query.stazione, req.query.numero);
-   	res.send(JSON.stringify(data));
-	// console.log("/info response in: ", timestamp() - time, "ms");
-});
+
+// app.get("/news", async (req, res) => { //http://localhost:3000/news
+// 	const data = await getNews();
+//    	res.send(JSON.stringify(data));
+// });
+
+// app.get("/treni", async (req, res) => { //http://localhost:3000/treni?tratta=2710/2715
+// 	const data = await getTratta(req.query.tratta);
+//    	res.send(JSON.stringify(data));
+// });
+
+// app.get("/info", async (req, res) => { //http://localhost:3000/info?stazione=2710&numero=16801
+// 	const data = await getTrain(req.query.stazione, req.query.numero);
+//    	res.send(JSON.stringify(data));
+// 	// console.log("/info response in: ", timestamp() - time, "ms");
+// });
 
 // app.get("/test", async (req, res) => { //http://localhost:3000/test
 // 	const data = await getTest();
 //    	res.send(JSON.stringify(data));
 // });
 
-app.listen(PORT, () => console.log("Server started"));
 
 // const perf = async (cb) => {
 // 	const time = timestamp();
--- treni.js
+++ treni.js
@@ -6,8 +6,20 @@ let CACHED_TRAINS = {};
 let CACHED_ROUTES = {};
 let CACHED_NUMERO = {};
 
+const SHORTED = {
+  "VENEZIA SANTA LUCIA": "VENEZIA S. LUCIA",
+  "DEVIATOIO ESTREMO CONEGLIANO": "CONEGLIANO",
+  "VENEZIA PORTO MARGHERA": "P. MARGHERA",
+  "DEVIATOIO ESTREMO VENEZIA": "VENEZIA S. LUCIA",
+  "VENEZIA MESTRE": "ST. MESTRE",
+  "CONFLUENZA UD-TS": "MESTRE",
+  "MOGLIANO VENETO": "MOGLIANO",
+  "S.GIOVANNI AL NATISONE": "S. GIOVANNI",
+  "RONCHI DEI LEGIONARI NORD" : "RONCHI",
+  "PONTE NELLE ALPI POLPET": "P. NELLE ALPI"
+}
+
 const timestamp = () => new Date().getTime();
-//  let stazioniString = (d.getHours() < 10 || (d.getHours() == 10 && d.getMinutes() < 30) ? selezionata + "/2712" : "2712/" + selezionata)
 
 export const getTratta = async (tratta) =>  {
   const route = CACHED_ROUTES[tratta];
@@ -19,9 +31,10 @@ export const getTratta = async (tratta) =>  {
 }
 
 const fetchTratta = async (tratta) => {
-  let date_format_str = moment().format('YYYY-MM-DDT4:00:00');
-  let response = await fetch(`http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/soluzioniViaggioNew/${tratta}/${date_format_str}`);
-  let data = await response.json()
+  const date_format_str = moment().format('YYYY-MM-DDT4:00:00');
+  const url = `http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/soluzioniViaggioNew/${tratta}/${date_format_str}`
+  const response = await fetch(url);
+  const data = await response.json()
 
   let treni = data?.soluzioni || []
 
@@ -31,9 +44,24 @@ const fetchTratta = async (tratta) => {
   }
 }
 
-export const getTrain = async (tratta, numero) => {
-  const route = CACHED_TRAINS[numero];
+const short = (val) => {
+  let shorted = SHORTED[val];
   
+  if (!shorted && val.length > 16)
+    console.log("Stringa troppo lunga: ", val);
+
+  return shorted || val;
+}
+
+export const getTrain = async (tratta, numero) => {
+
+  if (Number.isNaN(numero)) {
+    console.log("Numero treno non valido");
+    return;
+  }
+    
+  const route = CACHED_TRAINS[numero];
+
   if (!route || timestamp() - route.last_update > REFRESH_TIME)
     await fetchTrain(tratta, numero);
 
@@ -41,42 +69,62 @@ export const getTrain = async (tratta, numero) => {
 }
 
 const fetchNumero = async (numero) => {
+
+  // if () return; //validazione input
+
   let response = await fetch(`http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/cercaNumeroTreno/${numero}`).then(res => res.text())
-  if (response == "Error" || response == "") return
+  if (response.includes("Error") || response == "") return
   let { codLocOrig, dataPartenza } = JSON.parse(response);
 
   CACHED_NUMERO[numero] = {
     last_update: timestamp(),
     codLocOrig,
     dataPartenza
-  }
+  }   
 
 }
 const fetchTrain = async (tratta, numero) => { //2710 / 2715
+  
+  console.log(tratta)
+
+  if (numero == undefined || numero == "null") {
+    throw new Error(`Numero treno non valido (${numero})`); 
+  }
+  
   if (!CACHED_NUMERO[numero] || moment(CACHED_NUMERO[numero].last_update).date() != moment().date())
     await fetchNumero(numero);
 
   const { codLocOrig, dataPartenza } = CACHED_NUMERO[numero]
   
   let url = `http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/andamentoTreno/${codLocOrig}/${numero}/${dataPartenza}`
+  
+  console.log(url)
 
   let datiTreno = await fetch(url).then(res => res.json())
-  var { stazioneUltimoRilevamento, orarioPartenza, ritardo, oraUltimoRilevamento, nonPartito, fermate } = datiTreno;
 
-  let binario = fermate
-    .find((elem) => elem.id === `S0${tratta}`)
-    .binarioEffettivoArrivoDescrizione
+  var { stazioneUltimoRilevamento, ritardo, oraUltimoRilevamento, nonPartito, fermate } = datiTreno;
+
+  let codiceStazione = tratta.substring(0, 4);
+
+  let stazioneCorrente = fermate
+    .find((elem) => elem.id === `S0${codiceStazione}`)
+  
+  if (stazioneCorrente == undefined) throw new Error("Impossibile ricevere informazioni fermata treno. (probabilemente soppresso)")
+
+  let orarioPartenza = stazioneCorrente.partenza_teorica
+  // console.log(moment(orarioPartenza).format("HH:mm"), codiceStazione)
+  let binario = stazioneCorrente.binarioEffettivoArrivoDescrizione || stazioneCorrente.binarioProgrammatoArrivoDescrizione
 
   let rilevamento = moment(oraUltimoRilevamento).format('HH:mm')
 
-  let avanzate = `Ultimo rilevamento: ${stazioneUltimoRilevamento} (${rilevamento}) \nRitardo: ${ritardo} min \n`
+  let avanzate = `Ultimo rilevamento: ${short(stazioneUltimoRilevamento)} (${rilevamento}) \nRitardo: ${ritardo} min \n`
   
-  let programmato = moment(orarioPartenza).subtract(1, 'hour');
-  let previsto = programmato.add(ritardo, 'minutes');
+  let programmato = moment(orarioPartenza)
+  let previsto = moment(orarioPartenza + ritardo * 60000)
   programmato = programmato.format('HH:mm');
 
   var statotreno = (!nonPartito && stazioneUltimoRilevamento !== "--" ) ?
-    `${stazioneUltimoRilevamento} ${rilevamento}` :
+    `${short(stazioneUltimoRilevamento)} ${rilevamento}` :
     'Non ancora partito'
   
   avanzate += `Arrivo programmato: ${programmato}\n`;
@@ -84,7 +132,7 @@ const fetchTrain = async (tratta, numero) => { //2710 / 2715
   if (ritardo > 0)
     avanzate += `Arrivo previsto: ${previsto.format('HH:mm')}\n`;
 
-  avanzate += `Treno ${datiTreno.categoriaDescrizione} ${numero}`; 
+  avanzate += `Treno ${numero}`; //regionale
 
   const treniInfo = {
     programmato,
